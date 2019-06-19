55e129789a1fb6bee85ebd75657e960fcde7e085
Pablolo <pablo_olondriz@hotmail.com>
2019-06-19 13:37:23 +0200

    javascript code and else

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
+
+location1 = 3;
+location2 = 4;
+location3 = 5;
+
 let disparo = prompt ("Ready, aim, fire! (enter a number from 0 to 6");
+
+let hits = 0;
+let guesses = 0;
+let isSunk = false;
+
+
+if ( disparo < 0 || disparo > 6 ) {
+	alert ("Añade un número entre 0 y 6 para disparar!");
+} 
+
